8a21ecb3df51c40c3c69b69c82bca75f20fd2f0a
Feng Luan <feng.luan@bouvet.no>
2012-12-18 16:25:32 +0100

    add WriterTest

---
+++ WriterTest/src/main/java/bouvet/feng/WriterTest/App.java
@@ -0,0 +1,15 @@
+package bouvet.feng.WriterTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) throws Exception
+    {
+        System.out.println( "Hello World!" );
+        WriterTest test = new WriterTest();
+        test.runTest();
+    }
+}
